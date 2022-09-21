f49cb10bbbe161fc10f26cf76dd215824f5364ba
Gayusara25 <111350059+Gayusara25@users.noreply.github.com>
2022-09-21 11:38:23 +0530

    Add files via upload

---
+++ 19sep.sql
@@ -0,0 +1,57 @@
+create database Shopping
+
+create table Customer1
+(
+custid int primary key,
+custname varchar(20),
+city varchar(20),
+state varchar(20)
+)
+
+create table Products1
+(
+prodid int primary key,
+prodname varchar(20),
+unitprice int,
+unitofMeasurement float,
+QtyinStock int
+)
+
+create table sales_header1
+(
+invno int primary key,
+invdate int,
+invamt float,
+disPercent int
+)
+
+create table sales_detail1
+(
+invno int,
+custid int,
+prodid int,
+qtysold int
+)
+
+
+drop table dbo.Customer1
+drop table dbo.Products1
+drop table dbo.sales_detail1
+drop table dbo.sales_header1
+
+create table sales
+(invoice int primary key)
+
+
+alter table sales_detail1
+add foreign key(invno) references sales(invoice)
+
+alter table sales_detail1
+add foreign key(custid) references sales(invoice)
+add foreign key(prodid) references sales(invoice)
+
+alter table sales_detail1
+drop column qtysold
+
+alter table sales_detail1
+add Qty int
